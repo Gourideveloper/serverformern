08b4a25d34fd121443e1131d02569794707adee3
gourideveloper <gouri.developer@gmail.com>
2024-01-28 20:20:36 -0500

    update teja

---
+++ src/app.js
@@ -0,0 +1,8 @@
+
+export function App(){
+    return(
+        <div>
+        <h1>Welcome to react js</h1>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { App } from "./app";
+import {createRoot} from "react-dom/client";
+
+var root = createRoot(document.getElementById("root"));
+root.render(<App/>);
+
